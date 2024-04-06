f18921eeea25cb221b8a98bf84c7afe5e60239e4
mo1726 <mlahrech123@gmail.com>
2024-04-06 10:08:18 +0000

    add est api : put

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const app=experss();
 const port =3000;
 
 app.use(express.json());
+
+// get method
 app.get("/",(req , res) => {
     console.log("get Request Successfull!");
     res.send("GET Req Successrfully initiated");
 })
+
+// put method
+app.put("/put",(req , res) => {
+    console.log("PUT REQUEST SUCCESSFUL");
+    console.log(req.body);
+    res.send("DATA UPdate Request Recieved");
+})
+
+
 
 app.listen(port,()=>{
     console.log(`Server established at ${port}`);
